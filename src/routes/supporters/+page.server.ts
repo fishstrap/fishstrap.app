a565c0b8d80efdb00acce58817a1b75ee5b510a0
GoingCrazyDude <67791946+GoingCrazyDude@users.noreply.github.com>
2025-04-22 04:17:28 +0530

    finish up code

---
+++ src/routes/supporters/+page.server.ts
@@ -0,0 +1,69 @@
+/** @format */
+
+import type { PageServerLoad } from "./$types";
+import Cloudflare from "cloudflare";
+import { config } from 'dotenv';
+
+config();
+
+const NAMESPACE_ID = process.env.NAMESPACE_ID;
+const ACCOUNT_ID = process.env.ACCOUNT_ID;
+
+const client = new Cloudflare({
+    apiToken: process.env.API_TOKEN,
+});
+
+console.log(process.env.API_TOKEN)
+
+export const load: PageServerLoad = async () => {
+    // create empty arrays
+    let usernames: string[] = [];
+    let rawTransactionData: Array<any> = [];
+
+    const allUsernames: string[] = [];
+    const allTimestamps: string[] = [];
+    const allTypes: string[] = [];
+    const allAmounts: string[] = [];
+    const allCurrencies: string[] = [];
+
+    // get all users
+    for await (const key of client.kv.namespaces.keys.list(NAMESPACE_ID, { account_id: ACCOUNT_ID })) {
+        usernames.push(key.name);
+    }
+
+    // go through all the usernames and fetch the key values
+    for (const username of usernames) {
+        try {
+            const response = await client.kv.namespaces.values.get(NAMESPACE_ID, username, { account_id: ACCOUNT_ID });
+            const value = await response.json();
+            rawTransactionData.push({ username: username, value: value });
+        } catch (error) {
+            console.error(`Error fetching value for user ${username}:`, error);
+        }
+    }
+
+    // process data (this fucking sucks honestly)
+    rawTransactionData.forEach((item) => {
+        const username = item.username;
+        for (const timestamp in item.value) {
+            if (item.value.hasOwnProperty(timestamp)) {
+                const details = item.value[timestamp];
+                allUsernames.push(username);
+                allTimestamps.push(timestamp);
+                allTypes.push(
+                    details.subscription ? "Subscription" : "One-time",
+                );
+                allAmounts.push(details.amount);
+                allCurrencies.push(details.currency);
+            }
+        }
+    });
+
+    return {
+        usernames: allUsernames,
+        timestamps: allTimestamps,
+        types: allTypes,
+        amounts: allAmounts,
+        currencies: allCurrencies,
+    };
+};
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,7 @@
 import adapter from '@sveltejs/adapter-auto';
+import { loadEnv } from "vite";
+
+const env = loadEnv(process.env.NODE_ENV || 'development', process.cwd(), '');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -6,7 +9,10 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		env: {
+			dir: '.',
+		}
 	}
 };
 

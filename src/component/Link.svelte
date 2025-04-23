bccb4832cbae3ba1ee751ddc59594dc93d7d9d5f
Alexa <67791946+GoingCrazyDude@users.noreply.github.com>
2025-04-23 19:35:29 +0530

    do it in an actually good way

--- src/component/Link.svelte
+++ src/component/Link.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
     interface Props {
         href?: string;
-        content?: string;
     }
 
-    let { href = "", content = "" }: Props = $props();
+    let { href = ""}: Props = $props();
 </script>
 
 <a target="_blank" class="hover:shadow-lg duration-250 text-astronaut-blue-600 hover:text-astronaut-blue-200 pointer-events-auto inline-flex" href={href}>
-    {content} <span class="ml-1" style="font-family: Fira Code Variable, monospace; font-weight: 700;">-></span>
+    <slot/> <span class="ml-1" style="font-family: Fira Code Variable, monospace; font-weight: 700;">-></span>
 </a>

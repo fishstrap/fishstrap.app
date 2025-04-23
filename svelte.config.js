19b079415185f0c5a3f432425a0bbec19f9477a1
Alexa <67791946+GoingCrazyDude@users.noreply.github.com>
2025-04-23 05:56:02 +0530

    fix enviroment variables

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
-import { loadEnv } from "vite";
-
-const env = loadEnv(process.env.NODE_ENV || 'development', process.cwd(), '');
+import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,9 +7,6 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
-		env: {
-			dir: '.',
-		}
 	}
 };
 

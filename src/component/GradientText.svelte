e4e59c47e7e0ae02f61608986ba4140116637c96
GoingCrazyDude <67791946+GoingCrazyDude@users.noreply.github.com>
2025-06-04 05:22:34 +0530

    why was this generated?

--- src/component/GradientText.svelte
+++ src/component/GradientText.svelte
@@ -1,11 +1,5 @@
 <!-- @format -->
 <script>
-    /**
-     * @typedef {Object} Props
-     * @property {import('svelte').Snippet} [children]
-     */
-
-    /** @type {Props} */
     let { children } = $props();
 </script>
 

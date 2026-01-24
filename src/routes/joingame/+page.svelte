6e4d10964dfbea232559720118958b4832b419b4
Alexa <67791946+GoingCrazyDude@users.noreply.github.com>
2026-01-24 19:39:29 +0530

    fix: redirect requests for old endpoint

---
+++ src/routes/joingame/+page.svelte
@@ -0,0 +1,18 @@
+<!-- @format -->
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import { page } from '$app/state'
+    
+    const placeId = page.url.searchParams.get("placeId");
+    const gameInstanceId = page.url.searchParams.get("gameInstanceId");
+    const launchData = page.url.searchParams.get("launchData");
+    
+    onMount(() => {
+        if (!placeId) {
+            window.location.href = "/";
+            return;
+        }
+        window.location.href = `${window.location.origin}/v1/joingame?placeId=${placeId}${gameInstanceId ? `&gameInstanceId=${gameInstanceId}` : ""}${launchData ? `&launchData=${launchData}` : ""}`;
+    });
+</script>
+<div class="bg-black" />

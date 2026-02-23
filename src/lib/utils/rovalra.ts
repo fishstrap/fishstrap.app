5c5afec3c39e3de423c1fdca2b9af8f1fdc30b80
return_request <wirek5045@gmail.com>
2026-02-23 11:00:13 +0100

    refactor roblox.ts code

---
+++ src/lib/utils/rovalra.ts
@@ -0,0 +1,22 @@
+import { version } from "../../../package.json";
+
+export async function fetchServerData(placeId: string, gameInstanceId: string) {
+    try {
+        const valraResponse = await fetch(
+            `https://apis.rovalra.com/v1/server_details?place_id=${placeId}&server_ids=${gameInstanceId}`,
+            {
+                method: "GET",
+                headers: {
+                    "User-Agent": `FishstrapWeb/${version}`,
+                },
+            },
+        );
+
+        if (!valraResponse.ok) throw new Error("valra your api sucks");
+
+        const valraData = await valraResponse.json();
+        return valraData;
+    } catch (error) {
+        console.error(error);
+    }
+}
